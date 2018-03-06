033552fd88e9ce3070cf53578cde9cfdca8d913b
Lucas Felgueiras <lucas_arthur_f@hotmail.com>
2018-03-06 11:49:05 -0300

    fix

--- db/migrate/20180306113841_add_status_to_texts.rb
+++ db/migrate/20180306113841_add_status_to_texts.rb
@@ -2,7 +2,7 @@ class AddStatusToTexts < ActiveRecord::Migration[5.1]
   def change
     add_column :texts, :status, :string
 
-    Texts.all.each do |text|
+    Text.all.each do |text|
       text.status = "done"
       text.save
     end
